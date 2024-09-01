794ae5b7dd005b7c9a32f3843878df2c13e3c2d8
John Doe <aurimas.butk@gmail.com>
2024-09-01 03:18:22 +0200

    add map with

---
+++ my-vue-leaflet-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Vue with Leaflet Map</h1>
+    <MapComponent />
+  </div>
+</template>
+
+<script>
+import MapComponent from './components/MapComponent.vue'; // Importuojame MapComponent
+
+export default {
+  components: {
+    MapComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-leaflet-app/src/components/MapComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="map" ref="map" style="height: 100%; width: 100%;"></div>
+  </template>
+  
+  <script>
+  import { onMounted, ref } from 'vue';
+  import L from 'leaflet'; // Įsitikinkite, kad šis importas yra teisingas
+  
+  export default {
+    setup() {
+      const mapElement = ref(null);
+  
+      onMounted(() => {
+        const map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    L.marker([51.505, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+          
+      });
+  
+      return {
+        mapElement,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  #map {
+    height: 100vh;
+      width: 100vw;
+  }
+  </style>
+  
