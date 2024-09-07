6d6c63dadbe8addefe6b74cf0ca5042f685a12be
John Doe <aurimas.butk@gmail.com>
2024-09-07 18:58:55 +0200

    add bootstrap

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,49 @@
 <template>
-    <Dashboard />
-  </template>
-  
-  <script setup>
-  import Dashboard from '~/components/Dashboard.vue'
-  </script>
-  
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+      <a class="navbar-brand" href="#">My Project</a>
+    </nav>
+
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">Hello, World!</h5>
+        <p class="card-text">Welcome to my Nuxt project with Bootstrap and Leaflet.</p>
+        <button class="btn btn-primary mr-2">Primary Button</button>
+        <button class="btn btn-success mr-2">Success Button</button>
+        <button class="btn btn-danger">Danger Button</button>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col">
+        <div id="map" style="height: 500px;"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let L;
+
+export default {
+  mounted() {
+    if (process.client) {
+      import('leaflet').then((module) => {
+        L = module.default;
+
+        const map = L.map('map').setView([54.6872, 25.2797], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+        }).addTo(map);
+      });
+    }
+  },
+};
+</script>
+
+<style scoped>
+#map {
+  height: 500px;
+}
+</style>
