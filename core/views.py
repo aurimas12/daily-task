9509749927e0201002d3c6d2113c7ec5171f38aa
Jurij Jusel <jjusel@gmail.com>
2024-09-29 21:47:11 +0300

    add media root

--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
+from rest_framework import viewsets
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import MyModel
+from .serializers import MyModelSerializer
 
-schema_view = get_swagger_view(title='Swagger')
+class MyModelViewSet(viewsets.ModelViewSet):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
-urlpatterns = [
-    url(r'^$', schema_view)
-]
+from django.conf import settings
+from django.http import JsonResponse
+from pathlib import Path
+import os
 
+def list_images(request):
+    images_dir = Path(settings.MEDIA_ROOT) / 'images'
+    image_files = [f.name for f in images_dir.iterdir() if f.is_file()]
+
+    image_paths = [settings.MEDIA_URL + 'images/' + image for image in image_files]
+
+    return JsonResponse({'images': image_paths})
